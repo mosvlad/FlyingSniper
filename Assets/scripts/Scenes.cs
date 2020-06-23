1d94b563352d4242d9253c0abd61dc6babd51a55
Kotenka <37335759+Kotenka@users.noreply.github.com>
2020-06-23 15:10:31 +0300

    Переходы

---
+++ Assets/scripts/Scenes.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;                  // не забудь подключить библиотеку для работы со сценами
+
+public class Scenes : MonoBehaviour
+{
+    public void ChooseScene(int a)                  // Метод смены сцены с номером сцены
+    {
+        SceneManager.LoadScene(a);                  // Выбор сцены (а может быть сценой 1, 2, 3 и т.д. - ну, там build setting и тд)
+    }
+}
